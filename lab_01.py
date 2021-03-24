a1e6d6a70490fdf57e22c606ec7117ffbc0989f5
benjamin gonzalez <bigonzalez@miuandes.cl>
2021-03-24 11:14:01 -0300

    areglando detalles y empazando a poner todo en ingles

--- lab_01.py
+++ lab_01.py
@@ -1,15 +1,14 @@
+# se recomienda poner la terminal mas grande o que se vea mas 
+#-------------------------------79---------------------------------------------
 # imports
-import  random, sys
+
 from random import shuffle
 from tabulate import tabulate
 from typing import Tuple
 
 # variables
-y = 1
+
 NumberOfCardsRemaining = 0
-parity = 3
-L = []
-T = []
 MyHeaders =['','0','1','2','3','4','5','6','7','8','9']
 
 class PLAYERS_TURNS:
@@ -21,7 +20,8 @@ class PLAYERS_TURNS:
 
 #funciones y definiciones
 def election(l):
-    print("elija la ",l,"° carta, seleccione las coordenadas")
+    print('elija la ',l,'° carta, seleccione las coordenadas\n'
+    '(choose the ', l,' ° card, select the coordinates)')
     electionX = int(input("seleccione la coordenada x: "))
     electionY = int(input("seleccione la coordenada y: "))
     print("coordenada en X: ", electionX, "coordenada en Y: ", electionY)
@@ -30,18 +30,17 @@ def election(l):
     return elections
 
 
-def GameTable(numcard, options):
+def GameSettings(numcard, options):
     if (options == 1): 
-        rand = random.randint(0,3)
         LCT = []
-        for h in range(numcard):
+        for censure in range(numcard):
             LCT.append('?')
             LCT.append('?')
         return LCT
     elif (options == 2): 
         y = 1
         LI = []
-        for m in range(numcard):
+        for chance in range(numcard):
             LI.append(y)
             LI.append(y)
             y += 1
@@ -49,18 +48,18 @@ def GameTable(numcard, options):
         return(LI)
     elif (options == 3): 
         Clear = []
-        for C in range(numcard):
+        for Empty in range(numcard):
             Clear.append('')
             Clear.append('')
         return Clear
 
-def CreatedTable(List,Line,MyHeaders):
+def ShowTable(List,Line,MyHeaders):
     Roster = []
     q = 0
     p = 10
     b = 0
     d = 10
-    for CT in range(Line):
+    for Show in range(Line):
         register = List[q:d]
         register.insert(0,b)
         Roster.append(register)
@@ -72,7 +71,7 @@ def CreatedTable(List,Line,MyHeaders):
 
 def PlayerPhase(P) -> Tuple[bool,bool]:
     print("jugador: ",P)
-    CreatedTable(Game,rows,MyHeaders)
+    ShowTable(Game,rows,MyHeaders)
     Ex = election(1)
     while(True):
         try:
@@ -82,10 +81,10 @@ def PlayerPhase(P) -> Tuple[bool,bool]:
             print('no existe el número en la tabla')
             print('intentelo otra vez')
             print('\n')
-            CreatedTable(Game,rows,MyHeaders)
+            ShowTable(Game,rows,MyHeaders)
             print('\n')
             Ex = election(1)
-    CreatedTable(Game,rows,MyHeaders)
+    ShowTable(Game,rows,MyHeaders)
     Xe = election(2)
     while(True):
         try:
@@ -95,15 +94,15 @@ def PlayerPhase(P) -> Tuple[bool,bool]:
             print('no existe el numero en la tabla')
             print('intentelo otra vez')
             print('\n')
-            CreatedTable(Game,rows,MyHeaders)
+            ShowTable(Game,rows,MyHeaders)
             print('\n')
             Xe = election(2)
-    CreatedTable(Game,rows,MyHeaders)
+    ShowTable(Game,rows,MyHeaders)
 
     if (Mace[Ex] == Mace[Xe]):
         Game[Ex] = ''
         Game[Xe] = ''
-        print("Ganaste, jugador:",P," , toma las cartas, ganas un punto y sigues")
+        print("Ganaste, jugador:",P," ,ganas un punto y sigues")
         return PLAYERS_TURNS.P1_P_P1_N if P == 1 else PLAYERS_TURNS.P2_P_P2_N
     Game[Ex] = '?'
     Game[Xe] = '?'
@@ -113,20 +112,20 @@ def PlayerPhase(P) -> Tuple[bool,bool]:
 print('\n')
 
 # jugadores
-player1 = 0
-player2 = 0
+Score1 = 0
+Score2 = 0
 
+print('BIENVENIDO A JUEGO EL MEMORIZE')
 print('\n')
-
 # seleccion de cantidad de cartas y otros
 card = int(input("elija la cantidad de cartas: "))
 cards = card * 2
 count = cards
 rows = 0
 
-Mace = GameTable(card,2)
-finish = GameTable(card,3)
-Game = GameTable(card,1)
+Mace = GameSettings(card,2)
+finish = GameSettings(card,3)
+Game = GameSettings(card,1)
 if(cards >= 10):
     rows = int(cards/10)
     if( rows != (cards%10 == 0)):
@@ -142,36 +141,36 @@ print('\n')
 
 play = PlayerPhase(1)
 if(play == PLAYERS_TURNS.P1_P_P1_N):
-    player1 += 1
-    print('puntaje del jugador1: ', player1)
+    Score1 += 1
+    print('puntaje del jugador1: ', Score1)
 while Game != finish:
     if(play == (True,True)):
-        player1 += 1
-        print('puntaje del jugador1: ', player1)
+        Score1 += 1
+        print('puntaje del jugador1: ', Score1)
         play = PlayerPhase(1)
     elif (play == (False,False)):
-        player2 += 1
-        print('puntaje del jugador2: ', player2)
+        Score2 += 1
+        print('puntaje del jugador2: ', Score2)
         play = PlayerPhase(2)
     elif (play == (True,False)):
-        print('puntaje del jugador2: ', player2)
+        print('puntaje del jugador2: ', Score2)
         play = PlayerPhase(2)
     elif (play == (False,True)):
-        print('puntaje del jugador1: ', player1)
+        print('puntaje del jugador1: ', Score1)
         play = PlayerPhase(1)
 print('\n')
 #puntajes y ganador
 
 print('Resolucion del puntaje')
-if (player1 > player2):
+if (Score1 > Score2):
     print("ganador: Jugador 1")
-    print('puntaje: jugador1: ',player1)
-    print('puntaje: jugador2: ',player2)
-elif (player1 < player2):
+    print('puntaje jugador1: ',Score1)
+    print('puntaje jugador2: ',Score2)
+elif (Score1 < Score2):
     print("ganador: jugador 2")
-    print('puntaje: jugador1: ',player1)
-    print('puntaje: jugador2: ',player2)
+    print('puntaje jugador1: ',Score1)
+    print('puntaje jugador2: ',Score2)
 else:
     print("empate")
-    print('puntaje: jugador1: ',player1)
-    print('puntaje: jugador2: ',player2)
+    print('puntaje jugador1: ',Score1)
+    print('puntaje jugador2: ',Score2)
