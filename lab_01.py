cdbb1820c8cb8d8d4032c79ba9abca0d4d9e54f0
benjamin gonzalez <bigonzalez@miuandes.cl>
2021-03-24 12:09:34 -0300

    minimos detalles agregados y  todo en ingles

--- lab_01.py
+++ lab_01.py
@@ -20,11 +20,15 @@ class PLAYERS_TURNS:
 
 #funciones y definiciones
 def election(l):
+    #ojo con los strings y las comillas
     print('elija la ',l,'° carta, seleccione las coordenadas\n'
-    '(choose the ', l,' ° card, select the coordinates)')
-    electionX = int(input("seleccione la coordenada x: "))
-    electionY = int(input("seleccione la coordenada y: "))
-    print("coordenada en X: ", electionX, "coordenada en Y: ", electionY)
+    '(choose the ', l,' ° card, select the coordinates)\n')
+    electionX = int(input('seleccione la coordenada x: \n'
+    '(select x coordinate): '))
+    electionY = int(input('seleccione la coordenada y: \n'
+    '(select y coordinate):  '))
+    print("coordenada en X: ",electionX, "coordenada en Y: ",electionY, '\n'
+    '(X coordinate: ',electionX,' Y coordinate: ',electionY, ')\n')
     print('(',electionX,',',electionY,')')
     elections = (electionX*10+electionY)
     return elections
@@ -78,8 +82,10 @@ def PlayerPhase(P) -> Tuple[bool,bool]:
             Game[Ex] = Mace[Ex]
             break
         except:
-            print('no existe el número en la tabla')
-            print('intentelo otra vez')
+            print('no existe el número en la tabla \n'
+            '(there is no number in the table)')
+            print('intentelo otra vez \n'
+            '(try again)')
             print('\n')
             ShowTable(Game,rows,MyHeaders)
             print('\n')
@@ -91,8 +97,10 @@ def PlayerPhase(P) -> Tuple[bool,bool]:
             Game[Xe] = Mace[Xe]
             break
         except:
-            print('no existe el numero en la tabla')
-            print('intentelo otra vez')
+            print('no existe el número en la tabla \n'
+            '(there is no number in the table)')
+            print('intentelo otra vez \n'
+            '(try again)')
             print('\n')
             ShowTable(Game,rows,MyHeaders)
             print('\n')
@@ -102,11 +110,13 @@ def PlayerPhase(P) -> Tuple[bool,bool]:
     if (Mace[Ex] == Mace[Xe]):
         Game[Ex] = ''
         Game[Xe] = ''
-        print("Ganaste, jugador:",P," ,ganas un punto y sigues")
+        print('Ganaste, jugador: ',P,' ,ganas un punto y sigues \n'
+        '(You won, player:', P, ', you win a point and continue)\n')
         return PLAYERS_TURNS.P1_P_P1_N if P == 1 else PLAYERS_TURNS.P2_P_P2_N
     Game[Ex] = '?'
     Game[Xe] = '?'
-    print("perdiste, jugador ",P," , pasa el siguiente jugador")
+    print('perdiste, jugador: ',P,' , pasa el siguiente jugador \n'
+    '(you lost, player:', P, ', pass the next player)\n')
     return PLAYERS_TURNS.P1_P_P2_N if P == 1 else PLAYERS_TURNS.P2_P_P1_N
 
 print('\n')
@@ -115,12 +125,13 @@ print('\n')
 Score1 = 0
 Score2 = 0
 
-print('BIENVENIDO A JUEGO EL MEMORIZE')
+print('BIENVENIDO A JUEGO EL MEMORICE\n'
+'(WELCOME TO THE MEMORICE GAME)')
 print('\n')
 # seleccion de cantidad de cartas y otros
-card = int(input("elija la cantidad de cartas: "))
+card = int(input('elija la cantidad de cartas: \n'
+'(choose the number of cards): '))
 cards = card * 2
-count = cards
 rows = 0
 
 Mace = GameSettings(card,2)
@@ -130,47 +141,62 @@ if(cards >= 10):
     rows = int(cards/10)
     if( rows != (cards%10 == 0)):
         rows += 1
-    print('se han generado: ',rows,'filas')
+    print('se han generado: ',rows,'filas \n'
+    '(have been generated:', rows, 'rows)')
 else:
     rows += 1
 
 #print('se han generado: ',len(MyHeaders)-1,'columnas [del 0 al 9]')
-print('cantidad de cartas: ,cards')
+print('cantidad de cartas: ',cards,'\n'
+'(number of cards:', cards,')')
 print('\n')
 #juego
 
 play = PlayerPhase(1)
 if(play == PLAYERS_TURNS.P1_P_P1_N):
     Score1 += 1
-    print('puntaje del jugador1: ', Score1)
+    print('puntaje del jugador1: ', Score1,'\n'
+    '(player1 score:', Score1,')')
 while Game != finish:
     if(play == (True,True)):
         Score1 += 1
-        print('puntaje del jugador1: ', Score1)
+        print('puntaje del jugador1: ', Score1,'\n'
+        '(player1 score:', Score1,')')
         play = PlayerPhase(1)
     elif (play == (False,False)):
         Score2 += 1
-        print('puntaje del jugador2: ', Score2)
+        print('puntaje del jugador2: ', Score2, '\n'
+        '(player 2 score:', Score2,')')
         play = PlayerPhase(2)
     elif (play == (True,False)):
-        print('puntaje del jugador2: ', Score2)
+        print('puntaje del jugador2: ', Score2, '\n'
+        '(player 2 score:', Score2,')')
         play = PlayerPhase(2)
     elif (play == (False,True)):
-        print('puntaje del jugador1: ', Score1)
+        print('puntaje del jugador1: ', Score1,'\n'
+        '(player1 score:', Score1,')')
         play = PlayerPhase(1)
 print('\n')
 #puntajes y ganador
 
-print('Resolucion del puntaje')
+print('Resolucion del puntaje\n'
+'Score resolution')
 if (Score1 > Score2):
     print("ganador: Jugador 1")
-    print('puntaje jugador1: ',Score1)
-    print('puntaje jugador2: ',Score2)
+    print('puntaje jugador1: ',Score1,'\n'
+    '(player1 score:', Score1,')')
+    print('puntaje jugador2: ',Score2,'\n'
+    '(player 2 score:', Score2,')')
 elif (Score1 < Score2):
     print("ganador: jugador 2")
-    print('puntaje jugador1: ',Score1)
-    print('puntaje jugador2: ',Score2)
+    print('puntaje jugador1: ',Score1,'\n'
+    '(player1 score:', Score1,')')
+    print('puntaje jugador2: ',Score2,'\n'
+    '(player 2 score:', Score2,')')
 else:
-    print("empate")
-    print('puntaje jugador1: ',Score1)
-    print('puntaje jugador2: ',Score2)
+    print('empate\n'
+    'tie')
+    print('puntaje jugador1: ',Score1,'\n'
+    '(player1 score:', Score1,')')
+    print('puntaje jugador2: ',Score2,'\n'
+    '(player 2 score:', Score2,')')
