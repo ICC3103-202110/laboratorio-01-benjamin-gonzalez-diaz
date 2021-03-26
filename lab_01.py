e61e64dd5b464383dae3466fcc1639b4afcec27f
benjamin gonzalez <bigonzalez@miuandes.cl>
2021-03-26 10:46:52 -0300

    ajuste pre finales

--- lab_01.py
+++ lab_01.py
@@ -1,5 +1,7 @@
-# se recomienda poner la terminal mas grande o que se vea mas 
+# se recomienda poner la terminal lo mas grande, para poder ver toda la tabla
 #-------------------------------79---------------------------------------------
+# se asume que el jugador no va a volver a elegir las cartas que ya quito
+# con 'locked' me refiero a que no podra seguir hasta que cumpla la condicion
 # imports
 
 from random import shuffle
@@ -80,38 +82,62 @@ def ShowTable(List,Line,MyHeaders):
     print(tabulate(Roster,headers=MyHeaders,tablefmt='fancy_grid'))
 
 #las fases de los jugadores, el cual depende del numero del jugador
+# cada vez que sale el ShowTable se muestra la tabla
 def PlayerPhase(P) -> Tuple[bool,bool]:
     print("jugador: ",P)
-    ShowTable(Game,rows,MyHeaders)
-    Ex = election(1)
-    while(True):
+    ShowTable(Game,rows,MyHeaders) 
+    Ex = election(1) 
+    while(True):# un while 'locked' para evitar que el jugador elija una coordenada
+                # inexistente
         try:
-            Game[Ex] = Mace[Ex]
+            Game[Ex] = Mace[Ex] #se compara la coordenada con la tabla
             break
         except:
-            print('no existe el número en la tabla \n'
-            '(there is no number in the table)\n')
+            #cuando no esta el numero en la tabla
+            print('no existe el espacio para numero en la tabla \n'
+            '(there is no space for number in the table)\n')
             print('intentelo otra vez \n'
             '(try again)')
             print('\n')
             ShowTable(Game,rows,MyHeaders)
             print('\n')
-            Ex = election(1)
+            Ex = election(1) #se le vuelve a repetie la eleccion
     ShowTable(Game,rows,MyHeaders)
     Xe = election(2)
-    while(True):
+    while(True): # lo mismo que en el while de arriba para la 2°carta
         try:
             Game[Xe] = Mace[Xe]
             break
         except:
-            print('no existe el número en la tabla \n'
-            '(there is no number in the table)\n')
+            print('no existe el espacio para numero en la tabla \n'
+            '(there is no space for number in the table)\n')
             print('intentelo otra vez \n'
             '(try again)')
             print('\n')
             ShowTable(Game,rows,MyHeaders)
             print('\n')
             Xe = election(2)
+    while(Ex == Xe):
+        print('ojo con elegir la misma coordenada\n' # para evitar elegir la misma coordenada y ganar un punto
+        'be careful with choosing the same coordinate\n')
+        ShowTable(Game,rows,MyHeaders)
+        Xe = election(2) #se vuelve a repetir la segunda eleccion (elegir la segunda carta)
+        while(True):
+            try:
+                Game[Xe] = Mace[Xe]
+                break
+            except:
+                print('no existe el número en la tabla \n'
+                '(there is no number in the table)\n')
+                print('intentelo otra vez \n'
+                '(try again)')
+                print('\n')
+                ShowTable(Game,rows,MyHeaders)
+                print('\n')
+                Xe = election(2)
+        if(Ex != Xe):
+            break
+
     ShowTable(Game,rows,MyHeaders)
     #se usa una clase para retornar los bool, bool
     if (Mace[Ex] == Mace[Xe]):
@@ -140,7 +166,7 @@ print('\n')
 # seleccion de cantidad de cartas y otros
 card = int(input('elija la cantidad de cartas: \n'
 '(choose the number of cards): '))
-# un while "lock" para obligar al jugador que ponga un numero mayor a 0
+# un while "locked" para obligar al jugador que ponga un numero mayor a 0
 while (True):
     if (card < 1):
         print('elija una cantidad mayor a 0\n'
@@ -154,9 +180,9 @@ while (True):
 cards = card * 2
 rows = 0
 
-Mace = GameSettings(card,2)
-finish = GameSettings(card,3)
-Game = GameSettings(card,1)
+Mace = GameSettings(card,2) #tabla con numeros
+finish = GameSettings(card,3)#tabla vacia
+Game = GameSettings(card,1) #tabla censurada
 #------------------------
 #pequeño algoritmo para ver la cantidad de filas que deberia tener la tabla (por ejemplo si el usuario pide
 #13 cartas va seran 2 filas (10 columnas - 4 columnas)
@@ -171,6 +197,7 @@ else:
     rows += 1
 
 # se muestran la cantidad de cartas totales (no las que ingreso el usuario)
+# si no las cartas del tablero (que serian 2 veces las cartas que ingreso el usuario)
 print('cantidad de cartas: ',cards,'\n'
 '(number of cards:', cards,')')
 print('\n')
@@ -183,6 +210,8 @@ if(play == PLAYERS_TURNS.P1_P_P1_N):
     print('puntaje del jugador1: ', Score1,'\n'
     '(player1 score:', Score1,')')
 while Game != finish:
+    #se añaden los puntos al ganar 
+    # y no se añade nada al perder
     if(play == (True,True)):
         Score1 += 1
         print('puntaje del jugador1: ', Score1,'\n'
@@ -209,7 +238,7 @@ print('Resolucion del puntaje\n'
 if (Score1 > Score2):
     Score1 += 1
     print("GANADOR: --> Jugador 1 <-- \n"
-    '(WINNER: --> Player 1 <--)')
+    '(WINNER: --> Player 1 <--) \n\n')
     print('puntaje jugador1: ',Score1,'\n'
     '(player1 score:', Score1,')')
     print('puntaje jugador2: ',Score2,'\n'
@@ -217,14 +246,14 @@ if (Score1 > Score2):
 elif (Score1 < Score2):
     Score2 += 1
     print("GANADOR: -->jugador 2 <--\n"
-    '(WINNER: --> Player 2 <--)')
+    '(WINNER: --> Player 2 <--)\n\n')
     print('puntaje jugador1: ',Score1,'\n'
     '(player1 score:', Score1,')')
     print('puntaje jugador2: ',Score2,'\n'
     '(player 2 score:', Score2,')')
 else:
     print('EMPATE\n'
-    'TIE')
+    'TIE\n')
     print('puntaje jugador1: ',Score1,'\n'
     '(player1 score:', Score1,')')
     print('puntaje jugador2: ',Score2,'\n'
